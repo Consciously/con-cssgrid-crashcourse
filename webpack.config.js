075bb46bdeb24068d538371176dcdcd29c0cc89f
Stefan Ihle <stefanihle@gmx.net>
2022-03-27 13:49:22 +0200

    create css grid crashcourse sandbox and boilerplate of css grid project

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin =
+	require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const htmlPageNames = ['template', 'grid2'];
+const multipleHtmlPlugins = htmlPageNames.map(name => {
+	return new HtmlWebpackPlugin({
+		template: `src/${name}.html`,
+		filename: `${name}.html`,
+		chunks: [`${name}`],
+		excludeChunks: ['index']
+	});
+});
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		bundle: path.resolve(__dirname, 'src/index.js'),
+		grid2: path.resolve(__dirname, 'src/grid2.js'),
+		template: path.resolve(__dirname, 'src/template.js')
+	},
+	devtool: 'source-map',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name][contenthash].js',
+		clean: true,
+		assetModuleFilename: '[name][ext]'
+	},
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, 'dist')
+		},
+		port: 3000,
+		open: true,
+		hot: true,
+		compress: true,
+		historyApiFallback: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource'
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'CSS Grid Crashcourse',
+			filename: 'index.html',
+			template: 'src/index.html'
+		})
+		// new BundleAnalyzerPlugin()
+	].concat(multipleHtmlPlugins)
+};
